797a2e4f011bafb4fcf0306ee5f4ea70d4693cf1
TheDonDope <idontreallyneedthese@gmail.com>
2016-11-26 16:07:08 +0100

    chore(release): 1.2.0

---
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,2],{378:function(n,o){},379:function(n,o){},380:function(n,o){},676:function(n,o,c){c(380),c(378),n.exports=c(379)}},[676]);
+//# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
